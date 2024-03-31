e4547958c4778e9483d2aa24fbced686b8aefafa
picachuu <jannica_allison_lim@dlsu.edu.ph>
2024-03-31 18:19:51 +0800

    visual bug

--- NodeJS/public/assets/js/techlite.js
+++ NodeJS/public/assets/js/techlite.js
@@ -280,26 +280,23 @@ function getIsManager() {
         }
 
 
-        // Check if the user is not logged in
-        if (!logged) {
-            // hide navigation bar profile image and button, id = navbarProfileImage, navbarProfile
-            document.getElementById('navbarProfileImage').style.display = 'none';
-            document.getElementById('navbarProfile').style.display = 'none';
-        }
-        // Check if the user is logged in
-        if (logged) {
-            // hide login button, id = loginBtn
-            document.getElementById('loginBtn').style.display = 'none';
-        }
 
-        // Check if the navbarProfileImage exists, and replace with user image accordingly
-        let navbarProfileImage = document.getElementById('navbarProfileImage');
-        if (navbarProfileImage) {   
-            // check if url is empty, if so keep default
-            if (img_url !== "") {
-                navbarProfileImage.src = img_url;
+            if (logged) {
+                document.querySelectorAll('.profileli').forEach(function(elem) {
+                    elem.style.display = 'block';
+                });
+                document.querySelectorAll('.logli').forEach(function(elem) {
+                    elem.style.display = 'none';
+                });
+            } else {
+                document.querySelectorAll('.profileli').forEach(function(elem) {
+                    elem.style.display = 'none';
+                });
+                document.querySelectorAll('.logli').forEach(function(elem) {
+                    elem.style.display = 'block';
+                });
             }
-        }
+        
 
         // Change behavior based on user role
         if (is_manager) {
