7db3ac22f5b89dde546bea77eb7566351568f07c
Niconicolo <niccolo_jimenez@dlsu.edu.ph>
2024-04-03 22:14:42 +0800

    deploymentTes

--- NodeJS/models/dbmodel.js
+++ NodeJS/models/dbmodel.js
@@ -20,7 +20,7 @@ const tier3_schedCollection = "tier3_sched";
 const userInfoCollection = "user_info";
 const userReservationsCollection = "user_reservation";
 //const seatCollection = "seat";
-const db_url = "mongodb://127.0.0.1:27017/";
+const db_url = "mongodb+srv://admin:12345@cluster0.fchjyy3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const mongo_uri = db_url+databaseName;
 
 
