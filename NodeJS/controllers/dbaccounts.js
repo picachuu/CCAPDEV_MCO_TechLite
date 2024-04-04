eeb525ea3a28b9acfeb5cef6b116b63f05d69347
picachuu <jannica_allison_lim@dlsu.edu.ph>
2024-04-04 23:18:51 +0800

    response design

--- NodeJS/controllers/dbaccounts.js
+++ NodeJS/controllers/dbaccounts.js
@@ -415,7 +415,7 @@ function add(server,modules){
             layout: 'index',
             title: 'TechLite - Delete Account',
             prompt: 'Successful',
-            message: 'Thanks for having us!'
+            message: 'Your account with TechLite has been successfully deleted. Thanks for having us!'
           });
         });
       }
@@ -471,7 +471,7 @@ function add(server,modules){
               layout: 'index',
               title: 'TechLite - Delete Account',
               prompt: 'Successful',
-              message: 'Thanks for having us!'
+              message: 'Your account with TechLite has been successfully deleted. Thanks for having us!'
             });
           });
         }
